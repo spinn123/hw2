1cebc65107c927904c1ce4f48b10c50c4e652b75
spinn123 <sarah.pinner@kellogg.northwestern.edu>
2022-05-06 02:40:42 +0000

    progress

--- kmdb.rb
+++ kmdb.rb
@@ -72,6 +72,9 @@
 # TODO!
 
 Movie.destroy_all
+Studio.destroy_all
+Actor.destroy_all
+Role.destroy_all
 
 # Generate models and tables, according to the domain model.
 # TODO!
@@ -83,33 +86,84 @@ Movie.destroy_all
 # Do not use hard-coded foreign key IDs.
 # TODO!
 
+#Studio
+new_studio = Studio.new
+new_studio["name"]= "Warner Bros"
+new_studio.save
+
 #Movie
 new_movie = Movie.new
 new_movie["title"]= "Batman Begins"
 new_movie["rating"]= "PG-13"
 new_movie["year_released"]= "2005"
-#new_movie["studio_id"]= warner_bros["id"]
+new_movie["studio_id"]= new_studio["id"]
 new_movie.save
 
 new_movie2 = Movie.new
 new_movie2["title"]= "The Dark Knight"
 new_movie2["rating"]= "PG-13"
 new_movie2["year_released"]= "2008"
-#new_movie2["studio_id"]= warner_bros["id"]
+new_movie2["studio_id"]= new_studio["id"]
 new_movie2.save
 
 new_movie3 = Movie.new
-new_movie3["title"]= " The Dark Knight Rises"
+new_movie3["title"]= "The Dark Knight Rises"
 new_movie3["rating"]= "PG-13"
 new_movie3["year_released"]= "2012"
-#new_movie3["studio_id"]= warner_bros["id"]
+new_movie3["studio_id"]= new_studio["id"]
 new_movie3.save
 
-#Studio
-new_studio = Studio.new
-new_studio["name"]= "Warner Bros"
-new_studio.save
+#Actor
+new_actor = Actor.new
+new_actor["name"]= "Christian Bale"
+new_actor.save
 
+new_actor2 = Actor.new
+new_actor2["name"]= "Michael Caine"
+new_actor2.save
+
+new_actor3 = Actor.new
+new_actor3["name"]= "Liam Neeson"
+new_actor3.save
+
+new_actor4 = Actor.new
+new_actor4["name"]= "Katie Holmes"
+new_actor4.save
+
+new_actor5 = Actor.new
+new_actor5["name"]= "Gary Oldman"
+new_actor5.save
+
+new_actor6 = Actor.new
+new_actor6["name"]= "Heath Ledger"
+new_actor6.save
+
+new_actor7 = Actor.new
+new_actor7["name"]= "Aaron Eckhart"
+new_actor7.save
+
+new_actor8 = Actor.new
+new_actor8["name"]= "Maggie Gyllenhaal"
+new_actor8.save
+
+new_actor9 = Actor.new
+new_actor9["name"]= "Tom Hardy"
+new_actor9.save
+
+new_actor10 = Actor.new
+new_actor10["name"]= "Joseph Gordon-Levitt"
+new_actor10.save
+
+new_actor11 = Actor.new
+new_actor11["name"]= "Anne Hathaway"
+new_actor11.save
+
+#Role 
+new_role = Role.new
+new_role["character_name"]= "Rachel Dawes"
+new_role["movie_id"] = new_movie["id"]
+new_role["actor_id"] = new_actor11["id"]
+new_role.save
 
 
 #can use data.rb bc that file is set up here
@@ -121,6 +175,16 @@ puts ""
 
 # Query the movies data and loop through the results to display the movies output.
 # TODO!
+
+all_movies = Movie.all
+for movie in all_movies 
+    studio = Studio.find_by({"id"=>movie["studio_id"]})
+    puts "#{movie["title"]} #{movie["rating"]} #{movie["year_released"]} #{studio["name"]}"
+end
+
+
+#thing = Thing.find_by({"id"=>1})
+
 
 # Prints a header for the cast output
 puts ""
